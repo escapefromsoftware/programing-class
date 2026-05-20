ab98fdc8fe711a28c5f5946e3d1890b609b16e04
escapefromsoftware <nono-3570@outlook.jp>
2026-05-20 20:45:15 +0900

    第6回課題

---
+++ No6-HW_EZ.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int count = 0;
+int main(){
+
+    int score[10] = {0};
+
+    for (int i = 0; i < 10 ;i++){
+        printf("%d 人目の点数を入力してください:",i+1);
+        scanf("%d",&score[i]);
+    }
+
+    printf("合格者は");
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 10; j++){
+            for(int k = 0; k < 10; k++){
+                if(score[j] > score[k]){
+                    count++;
+                }
+                else{
+                    continue;
+                }
+                
+            }
+            if(count >= 9){
+                printf("%d人目です。",j+1);
+                score[j] = -1;
+                count = 0;
+                break;
+            }
+            else{
+                count = 0;
+            }
+            
+        }
+    }
+
+return 0;
+}
