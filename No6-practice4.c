ba93f66ba63b739792d4d2df89be6ed45263ff13
escapefromsoftware <nono-3570@outlook.jp>
2026-05-21 10:22:17 +0900

    あ

--- No6-practice4.c
+++ No6-practice4.c
@@ -10,7 +10,7 @@ void main() {
     for (i = 0; i < NUM; i++)
     {
 
-        printf("%d?l???_??:",i+1);
+        printf("%d人目の点数:",i+1);
         scanf("%d",&score[i]);
 
     }
@@ -27,55 +27,15 @@ void main() {
             min_score = score[i];
         }
     }
-    printf("????_:%d ???_:%d\n",max_score,min_score);
+    printf("最大値:%d 最小値:%d\n",max_score,min_score);
 
     for(i = 0; i < NUM; i++)
     {
         new_score[i] = 50 * (score[i] - min_score) / (max_score - min_score) + 50;
-        printf("%d?l??:%d ?? %f\n",i+1,score[i],new_score[i]);
+        printf("%d:%d -> %f\n",i+1,score[i],new_score[i]);
     }
 
 
 }
 
-
-#include<stdio.h>
-#define NUM 10
-
-void main() {
-    int i;
-    int score[NUM];
-    float new_score[NUM];
-    int max_score, min_score;
-
-    for (i = 0; i < NUM; i++)
-    {
-
-        printf("%d?l???_??:",i+1);
-        scanf("%d",&score[i]);
-
-    }
-    max_score = score[0];
-    min_score = score[0];
-    for (i = 0; i < NUM; i++)
-    {
-        if(score[i] > max_score)
-        {
-            max_score = score[i];
-        }
-        if(score[i] < min_score)
-        {
-            min_score = score[i];
-        }
-    }
-    printf("????_:%d ???_:%d\n",max_score,min_score);
-
-    for(i = 0; i < NUM; i++)
-    {
-        new_score[i] = 50 * (score[i] - min_score) / (max_score - min_score) + 50;
-        printf("%d?l???_??:%d -> %f\n",i+1,score[i],new_score[i]);
-    }
-
-
-}
 
