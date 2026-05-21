fde4fba8b827f6ff6a7dc4642b57104224b918bb
escapefromsoftware <nono-3570@outlook.jp>
2026-05-21 12:50:57 +0900

    第6回練習課題7-2最適化

--- No6-practice7-2.c
+++ No6-practice7-2.c
@@ -9,36 +9,8 @@ int main(){
         case 0:
             a[n] = 3;
             break;
-        case 1:
+        default:
             a[n] = a[n-1]*2;
-            break;
-        case 2:
-            a[n] = a[n-1]*2;
-            break;
-        case 3:
-            a[n] = a[n-1]*2;
-            break;
-        case 4:
-            a[n] = a[n-1]*2;
-            break;
-        case 5:
-            a[n] = a[n-1]*2;
-            break;
-        case 6:
-            a[n] = a[n-1]*2;
-            break;
-        case 7:
-            a[n] = a[n-1]*2;
-            break;
-        case 8:
-            a[n] = a[n-1]*2;
-            break;
-        case 9:
-            a[n] = a[n-1]*2;
-            break;
-        case 10:
-            a[n] = a[n-1]*2;
-            break;
         }
         printf("%d\n", a[n]);
     }
