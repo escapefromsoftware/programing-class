08f02918ee8269aaf8399b1796a80202469dad2f
escapefromsoftware <nono-3570@outlook.jp>
2026-05-21 13:42:22 +0900

    e

--- No6-practice8.c
+++ No6-practice8.c
@@ -3,7 +3,9 @@
 int a[10] = {0, 2, 6};
 
 int main(){
-    for(int n = 2; n < 10; n++){
+    printf("%d\n", a[1]);
+    printf("%d\n", a[2]);
+    for(int n = 3; n < 10; n++){
         a[n] = a[n-1] + a[n-2] * 2 - 3;
         printf("%d\n", a[n]);
     }
--- No6-practice9.c
+++ No6-practice9.c
@@ -2,6 +2,8 @@
 
 int F[10] = {0, 1};
 int main(){
+    printf("%d\n", F[0]);
+    printf("%d\n", F[1]);
     for(int n = 2; n < 10; n++){
         F[n] = F[n-1] + F[n-2];
         printf("%d\n", F[n]);
